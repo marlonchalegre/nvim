c07cccf9e09c70a37739813eeb17ee15a147ef92
Marlon Chalegre <marlonchalegre@gmail.com>
2025-06-04 10:47:42 -0300

    Update avante config to use copilot
    Signed-off-by: Marlon Chalegre <marlonchalegre@gmail.com>

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -8,75 +8,11 @@ return {
     behaviour = {
       enable_cursor_planning_mode = true, -- enable cursor planning mode!
     },
-    openai = {
-      hide_in_model_selector = true,
-    },
-    claude = {
-      hide_in_model_selector = true,
-    },
-    aihubmix = {
-      hide_in_model_selector = true,
-    },
-    ["aihubmix-claude"] = {
-      hide_in_model_selector = true,
-    },
-    gemini = {
-      hide_in_model_selector = true,
-    },
-    vertex = {
-      hide_in_model_selector = true,
-    },
-    vertex_claude = {
-      hide_in_model_selector = true,
-    },
-    cohere = {
-      hide_in_model_selector = true,
-    },
-    bedrock = {
-      hide_in_model_selector = true,
-    },
-    ["claude-haiku"] = {
-      hide_in_model_selector = true,
-    },
-    ["claude-opus"] = {
-      hide_in_model_selector = true,
-    },
-    ["openai-gpt-4o-mini"] = {
-      hide_in_model_selector = true,
-    },
-    vendors = {
+    providers = {
       internal = {
         __inherited_from = "openai",
         endpoint = "http://127.0.0.1:8899/v1",
         model = "gpt-4.1", -- your desired model (or use gpt-4o, etc.)
-      },
-      copilot_claude_3_7 = {
-        __inherited_from = "copilot",
-        model = "claude-3.7-sonnet",
-      },
-      copilot_claude_3_7_thinking = {
-        __inherited_from = "copilot",
-        model = "claude-3.7-sonnet-thought",
-      },
-      copilot_gemini_2_0_flash = {
-        __inherited_from = "copilot",
-        model = "gemini-2.0-flash-001",
-      },
-      copilot_gemini_2_5_pro = {
-        __inherited_from = "copilot",
-        model = "gemini-2.5-pro",
-      },
-      copilot_openai_o4_mini = {
-        __inherited_from = "copilot",
-        model = "o4-mini",
-      },
-      copilot_openai_4_1 = {
-        __inherited_from = "copilot",
-        model = "gpt-4.1",
-      },
-      copilot_openai_o3_mini = {
-        __inherited_from = "copilot",
-        model = "o3-mini",
       },
     },
   },
